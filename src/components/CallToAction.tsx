10a5585d6ccde4db405e7c0d21bfad1276c9dffb
RishithRavi <76603172+RishithRavi@users.noreply.github.com>
2024-07-04 23:01:38 -0400

    Update CallToAction.tsx

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -14,7 +14,7 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-md sm:text-center">
           <h2 className="text-3xl font-medium tracking-tight text-white sm:text-4xl">
-            Start your investing journey today...
+            Start your investing journey today....
           </h2>
           <p className="mt-4 text-lg text-gray-300">
             It takes just 30 seconds to sign up. Download the app, create your account, and get your first tip to start your investment journey.
