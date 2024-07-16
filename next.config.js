40c43b21bbe3a79e33c16fd636dda6a5054a3649
Rishith Ravi <rishithravi26@gmail.com>
2024-07-15 22:21:47 -0400

    added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['www.levelingfinance.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  swcMinify: false,
+}
 
 module.exports = nextConfig
