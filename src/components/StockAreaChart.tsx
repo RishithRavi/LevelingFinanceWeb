66aa05c4df8b6f0b555fc97625c38f88f1a2fb51
Rishith Ravi <rishithravi26@gmail.com>
2024-07-17 14:49:56 -0400

    edited graph to show portfolio histroy

--- src/components/StockAreaChart.tsx
+++ src/components/StockAreaChart.tsx
@@ -1,5 +1,5 @@
-import { useClerk } from '@clerk/nextjs';
-import React, { useEffect, useState } from 'react';
+import { useClerk } from '@clerk/nextjs'
+import React, { useEffect, useState } from 'react'
 import {
   AreaChart,
   Area,
@@ -9,28 +9,28 @@ import {
   Tooltip,
   ResponsiveContainer,
   Text,
-} from 'recharts';
-import Loader from './Loader';
+} from 'recharts'
+import Loader from './Loader'
 
 interface DataType {
-  name: string;
-  close: number;
+  name: string
+  close: number
 }
 
 const StockAreaChart = () => {
   // const [token, setToken] = useState();
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState<DataType[]>([]);
-  const [has, setHas] = useState(false);
-  
-  const { session, user } = useClerk();
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState<DataType[]>([])
+  const [has, setHas] = useState(false)
+
+  const { session, user } = useClerk()
 
   useEffect(() => {
     const fetchData = async (token: string | null) => {
-      setLoading(true);
+      setLoading(true)
       try {
         const response = await fetch(
-          `https://api.levelingfinance.com/api/user/${user?.primaryPhoneNumber?.phoneNumber}/stocks/Msft/history`,
+          `https://api.levelingfinance.com/api/user/${user?.primaryPhoneNumber?.phoneNumber}/stocks/portfolio-history`,
           {
             headers: {
               'Content-Type': 'application/json',
@@ -39,81 +39,82 @@ const StockAreaChart = () => {
           },
         )
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
-        const json = await response.json();
-        const transformedData = transformData(json);
-        setData(transformedData);
-        setHas(true);
+        const json = await response.json()
+        const transformedData = transformData(json)
+        setData(transformedData)
+        setHas(true)
       } catch (error) {
-        console.error('Fetch error:', error);
-        setHas(false);
+        console.error('Fetch error:', error)
+        setHas(false)
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
-    };
+    }
 
     const transformData = (data: any[]) => {
-      const now = new Date();
-      const earliestDate = new Date(data[0].Date);
-      const diffTime = Math.abs(now.getTime() - earliestDate.getTime());
-      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  
+      const now = new Date()
+      const earliestDate = new Date(data[0].Date)
+      const diffTime = Math.abs(now.getTime() - earliestDate.getTime())
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+
       if (diffDays <= 90) {
-        return data.map((item: { Date: any; Close: any; }) => ({
+        return data.map((item: { Date: any; Close: any }) => ({
           name: formatDate(item.Date),
           close: item.Close,
-        }));
+        }))
       } else if (diffDays <= 365) {
         return data
           .filter((_: any, index: number) => index % 7 === 0)
-          .map((item: { Date: any; Close: any; }) => ({
+          .map((item: { Date: any; Close: any }) => ({
             name: formatDate(item.Date),
             close: item.Close,
-          }));
+          }))
       } else {
-        let lastMonth = -1;
-        let lastYear = -1;
+        let lastMonth = -1
+        let lastYear = -1
         return data
-          .filter((item: { Date: string | number | Date; }) => {
-            const date = new Date(item.Date);
-            const month = date.getMonth();
-            const year = date.getFullYear();
+          .filter((item: { Date: string | number | Date }) => {
+            const date = new Date(item.Date)
+            const month = date.getMonth()
+            const year = date.getFullYear()
             if (month !== lastMonth || year !== lastYear) {
-              lastMonth = month;
-              lastYear = year;
-              return true;
+              lastMonth = month
+              lastYear = year
+              return true
             }
-            return false;
+            return false
           })
-          .map((item: { Date: any; Close: any; }) => ({
+          .map((item: { Date: any; Close: any }) => ({
             name: formatDate(item.Date),
             close: item.Close,
-          }));
+          }))
       }
-    };
+    }
     session?.getToken().then((x) => {
-      fetchData(x);
-    });
-  }, [session, user,]);
-
+      fetchData(x)
+    })
+  }, [session, user])
 
   const formatDate = (dateString: string | number | Date) => {
-    const date = new Date(dateString);
-    const formattedDate = date.toLocaleDateString('en-US', { month: 'short', year: '2-digit' });
+    const date = new Date(dateString)
+    const formattedDate = date.toLocaleDateString('en-US', {
+      month: 'short',
+      year: '2-digit',
+    })
 
     if (date.getMonth() === 0) {
-      const month = date.toLocaleDateString('en-US', { month: 'short' });
-      return `${month} ${date.getFullYear()}`;
+      const month = date.toLocaleDateString('en-US', { month: 'short' })
+      return `${month} ${date.getFullYear()}`
     }
-    const newString =
-      formattedDate.slice(0, -2) + "'" + formattedDate.slice(-2);
-    return newString;
-  };
+    const newString = formattedDate.slice(0, -2) + "'" + formattedDate.slice(-2)
+    return newString
+  }
 
-  const CustomizedXAxisTick = ({ x, y, payload }:any) => {
-    const date = new Date(payload.value);
-    const isJanuary = date.getMonth() === 0;
+  const CustomizedXAxisTick = ({ x, y, payload }: any) => {
+    const date = new Date(payload.value)
+    const isJanuary = date.getMonth() === 0
     return (
       <Text
         x={x}
@@ -125,12 +126,14 @@ const StockAreaChart = () => {
         fill={isJanuary ? '#8884d8' : '#666'}
         fontSize={isJanuary ? 18 : 18}
       >
-        {!isJanuary ? payload.value.toString().slice(0, -3) : date.getFullYear()}
+        {!isJanuary
+          ? payload.value.toString().slice(0, -3)
+          : date.getFullYear()}
       </Text>
-    );
-  };
+    )
+  }
 
-  if (loading) return <Loader />;
+  if (loading) return <Loader />
 
   return (
     <ResponsiveContainer width="100%" height={450}>
@@ -159,7 +162,7 @@ const StockAreaChart = () => {
         />
       </AreaChart>
     </ResponsiveContainer>
-  );
-};
+  )
+}
 
-export default StockAreaChart;
+export default StockAreaChart
